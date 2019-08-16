a610976a929ca0eee9986f2ebe880c33b35c1084
Haithem Bel Haj <haithem@konek.to>
2019-08-16 15:38:52 +0200

    update deps to fix audits

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const selectAll = require('css-select');
 const selectOne = selectAll.selectOne;
 const serialize = require('dom-serializer');
 const svgToDataUri = require('mini-svg-data-uri');
-const svgo = new (require('svgo'));
+const svgo = new (require('svgo'))();
 const optimize = deasync(optimizeAsync);
 
-const defaultOptions = {optimize: false, encodingFormat: 'base64'}
+const defaultOptions = { optimize: false, encodingFormat: 'base64' };
 
 // exports
 module.exports = inliner;
@@ -25,24 +25,18 @@ module.exports = inliner;
  * @returns {Function}
  */
 function inliner(base, opts) {
-
   opts = assign({}, defaultOptions, opts);
 
-  return function(path, selectors){
-
+  return function(path, selectors) {
     let content = readFileSync(resolve(base, path.getValue()));
 
-    if(selectors && selectors.getLength && selectors.getLength())
-      content = changeStyle(content, selectors);
+    if (selectors && selectors.getLength && selectors.getLength()) content = changeStyle(content, selectors);
 
-    if(opts.optimize)
-      content = new Buffer(optimize(content).data);
+    if (opts.optimize) content = new Buffer(optimize(content).data);
 
-
-    return encode(content, {encodingFormat: opts.encodingFormat});
-  }
+    return encode(content, { encodingFormat: opts.encodingFormat });
+  };
 }
-
 
 /**
  * encode the string
@@ -50,21 +44,17 @@ function inliner(base, opts) {
  * @param opts
  * @returns {types.String}
  */
-function encode(content, opts){
-
+function encode(content, opts) {
   if (opts.encodingFormat === 'uri') {
-
-	  return (new types.String('url("'+svgToDataUri(content.toString('UTF-8'))+'")'));
+    return new types.String('url("' + svgToDataUri(content.toString('UTF-8')) + '")');
   }
 
   if (opts.encodingFormat === 'base64') {
-
-	  return (new types.String('url("data:image/svg+xml;base64,'+content.toString('base64')+'")'));
+    return new types.String('url("data:image/svg+xml;base64,' + content.toString('base64') + '")');
   }
 
   throw new Error('encodingFormat ' + opts.encodingFormat + ' is not supported');
 }
-
 
 /**
  * change the style of the svg
@@ -72,24 +62,21 @@ function encode(content, opts){
  * @param styles
  * @returns {*}
  */
-function changeStyle(source, selectors){
-
+function changeStyle(source, selectors) {
   const dom = parse(source, { xmlMode: true });
   const svg = dom ? selectOne('svg', dom) : null;
 
   selectors = mapToObj(selectors);
 
   if (!svg) {
-
     throw Error('Invalid svg file');
   }
 
-  Object.keys(selectors).forEach(function (selector) {
-
+  Object.keys(selectors).forEach(function(selector) {
     const elements = selectAll(selector, svg);
     let attribs = selectors[selector];
 
-    elements.forEach(function (element) {
+    elements.forEach(function(element) {
       assign(element.attribs, attribs);
     });
   });
@@ -102,25 +89,22 @@ function changeStyle(source, selectors){
  * @param map
  * @returns {null}
  */
-function mapToObj(map){
-
+function mapToObj(map) {
   const obj = Object.create(null);
 
-  for(let i = 0, len = map.getLength(); i < len; i++){
-
+  for (let i = 0, len = map.getLength(); i < len; i++) {
     const key = map.getKey(i).getValue();
     let value = map.getValue(i);
 
-    switch(value.toString()) {
-
+    switch (value.toString()) {
       case '[object SassMap]':
         value = mapToObj(value);
         break;
       case '[object SassColor]':
         if (value.getA() === 1) {
-          value = 'rgb('+value.getR()+','+value.getG()+','+value.getB()+')';
+          value = 'rgb(' + value.getR() + ',' + value.getG() + ',' + value.getB() + ')';
         } else {
-          value = 'rgba('+value.getR()+','+value.getG()+','+value.getB()+','+value.getA()+')';
+          value = 'rgba(' + value.getR() + ',' + value.getG() + ',' + value.getB() + ',' + value.getA() + ')';
         }
         break;
       default:
@@ -133,11 +117,11 @@ function mapToObj(map){
   return obj;
 }
 
-
-function optimizeAsync(src, cb){
-
-  svgo.optimize(src, function(result){
-
-    return cb(null, result)
-  })
+function optimizeAsync(src, cb) {
+  svgo
+    .optimize(src)
+    .then(function(result) {
+      return cb(null, result);
+    })
+    .catch(cb);
 }
